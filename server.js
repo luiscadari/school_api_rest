e2b1864eb5e8249a02a09a0405ebdfdb099a5541
luiscadari <luiscadari01@gmail.com>
2025-01-02 09:10:49 -0300

    :bricks: ci: configurando pastas do projeto

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
-import helloWorld from "./app";
-helloWorld();
+import app from "./app";
+
+const port = 3001;
+app.listen(port, () => {
+  console.log();
+  console.log("Escutando na porta: 3001");
+  console.log(`CTRL + click: http://locahost:${port}/`);
+});
