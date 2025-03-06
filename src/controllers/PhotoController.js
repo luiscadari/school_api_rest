4489e8866d9c30a47b0276376c580cdf30550102
luiscadari <luiscadari@icloud.com>
2025-03-06 14:34:36 -0300

    :recycle: refactor: photo upload

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,8 +1,15 @@
+import multerConfig from "../config/multerConfig";
+import multer from "multer";
+const upload = multer(multerConfig).single("photo");
+
 class PhotoController {
   async index(req, res) {
     return res.json({ success: "ok" });
   }
   async store(req, res) {
+    upload.single("file")(req, res, (err) => {
+      if (err) res.json({ error: "Tipo de arquivo inválido" });
+    });
     return res.json(req.file);
   }
 }
